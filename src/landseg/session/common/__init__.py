0f7d3f65018935a4d711b9695e95f39f57269cf3
Wenxi Feng <wenxi.feng@ontario.ca>
2026-04-16 14:08:35 -0400

    refactor/ rename protocols in common. module, i.e., remove 'engine_' prefix

--- src/landseg/session/common/__init__.py
+++ src/landseg/session/common/__init__.py
@@ -44,19 +44,19 @@ __all__ = [
 # for static check
 if typing.TYPE_CHECKING:
 
-    from .engine_comps import CompositeLossLike, ConfusionMatrixLike, SpecsLike, ComponentsLike
-    from .engine_config import ConfigLike
-    from .engine_state import StateLike
+    from .comps import CompositeLossLike, ConfusionMatrixLike, SpecsLike, ComponentsLike
+    from .config import ConfigLike
+    from .state import StateLike
 
 def __getattr__(name: str):
 
     if name in {'CompositeLossLike', 'ConfusionMatrixLike', 'SpecsLike', 'ComponentsLike'}:
-        return getattr(importlib.import_module('.engine_comps', __package__), name)
+        return getattr(importlib.import_module('.comps', __package__), name)
 
     if name in {'ConfigLike'}:
-        return getattr(importlib.import_module('.engine_config', __package__), name)
+        return getattr(importlib.import_module('.config', __package__), name)
 
     if name in {'StateLike'}:
-        return getattr(importlib.import_module('.engine_state', __package__), name)
+        return getattr(importlib.import_module('.state', __package__), name)
 
     raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
