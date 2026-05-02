6682c8c822f7cc9043eb9d0951995092541da633
Wenxi Feng <wenxi.feng@ontario.ca>
2026-05-01 23:03:27 -0400

    refactor/ add a toggle on whether user/dev yaml settings are to be resolved before execution. streamline config merging in API mode, where if running API, external dict-like custom config (e.g., a python dict in a notebook cell) is the only source of truth - user/dev yaml settings are ignored. These are exclusively to be used in CLI mode.

--- src/landseg/adapters/api.py
+++ src/landseg/adapters/api.py
@@ -25,6 +25,7 @@ Programmati API entry
 
 # standard imports
 import pathlib
+import typing
 # third-party imports
 import hydra
 import hydra.core.global_hydra
@@ -34,7 +35,7 @@ import landseg.execution as execution
 import landseg.utils as utils
 
 #
-def get_default_config(overrides: list[str]) -> omegaconf.DictConfig:
+def get_default_config(pipeline: str) -> omegaconf.DictConfig:
     '''
     Return a base Hydra-composed configuration for the given pipeline.
 
@@ -51,14 +52,29 @@ def get_default_config(overrides: list[str]) -> omegaconf.DictConfig:
 
     # compose config from existing configs from config/
     with hydra.initialize_config_dir(config_dir=cfg_dir,version_base='1.3'):
-        cfg = hydra.compose('config', overrides=overrides)
+        # here we apply preconfigured pipeline overrides
+        cfg = hydra.compose('config', overrides=[f'pipeline={pipeline}'])
+
+    # return
     return cfg
 
-def print_config(config: omegaconf.DictConfig):
-    '''doc'''
-    print(omegaconf.OmegaConf.to_yaml(config))
+def add_custom_config(
+    cfg: omegaconf.DictConfig,
+    custom_config: dict[str, typing.Any] | None = None
+) -> omegaconf.DictConfig:
+    '''Add custom config dictionary to root config.'''
 
-#
+    # early exit - no ops
+    if not custom_config:
+        return cfg
+    # create omega config dict from custom dict
+    custom = omegaconf.OmegaConf.create(custom_config)
+    # naively merge and we rely on downstream resolver for truthiness
+    with omegaconf.open_dict(cfg):
+        merged = omegaconf.OmegaConf.merge(cfg, custom)
+    assert isinstance(merged, omegaconf.DictConfig) # typing sanity
+    return merged
+
 def run(config: omegaconf.DictConfig):
     '''
     Run the selected pipeline using a pre-composed Hydra configuration.
@@ -71,8 +87,9 @@ def run(config: omegaconf.DictConfig):
     logger = utils.Logger('api', './api.log')
 
     try:
-        root_config = execution.resolve_configs(config)
-        return execution.execute_pipeline(root_config)
+        # here we rely on external caller to provide the only source of truth
+        cfg = execution.resolve_configs(config, use_additional_settings=False)
+        return execution.execute_pipeline(cfg)
     except KeyboardInterrupt:
         logger.log('INFO', 'Execution interrupted')
         raise
--- src/landseg/execution/resolver.py
+++ src/landseg/execution/resolver.py
@@ -32,51 +32,55 @@ import omegaconf
 # local imports
 import landseg.configs as configs
 
-# register omega resolvers
-omegaconf.OmegaConf.register_new_resolver("concat", lambda x, y: x + y)
+# aliases
+omega = omegaconf.OmegaConf
 
-def resolve_configs(config: omegaconf.DictConfig) -> configs.RootConfig:
+# register omega resolvers
+omega.register_new_resolver("concat", lambda x, y: x + y)
+
+def resolve_configs(
+    config: omegaconf.DictConfig,
+    use_additional_settings: bool = True
+) -> configs.RootConfig:
     '''Resolve configs from difference sources'''
 
     # list of configs to resolve
     config_list: list = []
 
     # add schema - dataclass scaffolding with default dummy values
-    schema = omegaconf.OmegaConf.structured(configs.RootConfig)
+    schema = omega.structured(configs.RootConfig)
     config_list.append(schema)
 
-    # add Hydra-composed config - this might also contain dummy values
+    # add Hydra-composed config
+    # - as the single source of truth in API mode
+    # - might override by additional settings (*yaml) below in CLI mode
     config_list.append(config)
 
     # add user settings - this should contain the complete config values
     # resolve absolute path to the user settings at root
     # root/src/landseg/execution/resolver.py -> the 4th parent
     user = pathlib.Path(__file__).resolve().parents[3] / 'settings.yaml'
-    if os.path.exists(user):
-        user_settings = omegaconf.OmegaConf.load(user)
+    if os.path.exists(user) and use_additional_settings:
+        user_settings = omega.load(user)
         assert isinstance(user_settings, omegaconf.DictConfig)
         config_list.append(user_settings)
-    else:
-        print(f"[WARN] user settings not provided: {user}")
 
     # add dev settings (optional and untracked)
-    dev = omegaconf.OmegaConf.select(config, 'execution.dev_settings', default=None)
-    if dev and os.path.exists(dev):
-        dev_settings = omegaconf.OmegaConf.load(dev)
+    dev = omega.select(config, 'execution.dev_settings', default=None)
+    if dev and os.path.exists(dev) and use_additional_settings:
+        dev_settings = omega.load(dev)
         assert isinstance(dev_settings, omegaconf.DictConfig)
         config_list.append(dev_settings)
-    else:
-        print(f"[INFO] dev settings not provided: {dev}")
 
     # merging configs in order (last wins)
     # dev -> user -> hydra defaults -> schema defaults
     with omegaconf.open_dict(config):
-        merged = omegaconf.OmegaConf.merge(*config_list)
+        merged = omega.merge(*config_list)
     cfg = typing.cast(omegaconf.DictConfig, merged)
-    omegaconf.OmegaConf.resolve(cfg)
+    omega.resolve(cfg)
 
     # construct and cast config dataclass
-    root = typing.cast(configs.RootConfig, omegaconf.OmegaConf.to_object(cfg))
+    root = typing.cast(configs.RootConfig, omega.to_object(cfg))
 
     # final validation checks before returning
     root.validate_all()
