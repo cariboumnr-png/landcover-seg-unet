3309eb067807f19e35636b9b9746b732e10df39d
Wenxi Feng <wenxi.feng@ontario.ca>
2026-05-24 21:48:36 -0400

    refactor/ add basic value validations (numerical range) to session configs

--- src/landseg/configs/_schema/session.py
+++ src/landseg/configs/_schema/session.py
@@ -41,11 +41,18 @@ class _DataLoaderConfig:
     patch_size: int = 128
     batch_size: int = 16
 
+    def validate(self):
+        _must_within(self.patch_size, 'data patch size', 0)
+        _must_within(self.batch_size, 'data batch size', 0)
+
 # ----- engione executor
 @dataclasses.dataclass
 class _EngineExecConfig:
     use_amp: bool = True
     logit_adjust_alpha: float = 1.0
+
+    def validate(self):
+        _must_within(self.logit_adjust_alpha, 'logit adjust alpha', 0)
 
 # ----- engine optim
 @dataclasses.dataclass
@@ -56,6 +63,15 @@ class _OptimConfig:
     sched_cls: str | None = 'CosAnneal'
     sched_args: dict[str, typing.Any] = field(default_factory=lambda: {'T_max': 50})
     grad_clip_norm: float | None = 1.0
+
+    def validate(self):
+        _must_within(self.lr, 'learning rate', 0)
+        _must_within(self.weight_decay, 'weight decay', 0)
+        _must_within(self.grad_clip_norm, 'gradient norm clipping', 0)
+        # scheduler specific requirements
+        if self.sched_cls == 'CosAnneal':
+            if 'T_max' not in self.sched_args:
+                raise ValueError('missing T_max for CosAnneal')
 
 # ----- engine tasks
 @dataclasses.dataclass
@@ -93,6 +109,13 @@ class _TasksConfig:
     excluded_cls: dict[str, list[int]] | None = None
     loss_types: _LossTypesConfig = field(default_factory=_LossTypesConfig)
 
+    def validate(self):
+        losses = self.loss_types
+        _must_within(losses.focal.weight, 'focal loss weight', 0)
+        _must_within(losses.dice.weight, 'dice loss weight', 0)
+        _must_within(losses.spectral.weight, 'spectral loss weight', 0)
+        _must_within(losses.tv.weight, 'tv loss weight', 0)
+
 # ----- orchestration
 @dataclasses.dataclass
 class _Schedule:
@@ -101,6 +124,12 @@ class _Schedule:
     ckpt_every_n_epoch: int = 5
     update_loss_every_n_batch: int = 50
     resume_from_last: bool = False
+
+    def validate(self):
+        _must_within(self.val_every_n_epoch, 'validation frequency', 1)
+        _must_within(self.infer_every_n_epoch, 'inference frequency', 1)
+        _must_within(self.ckpt_every_n_epoch, 'Saving frequency', 1)
+        _must_within(self.update_loss_every_n_batch, 'loss update frequency', 1)
 
 @dataclasses.dataclass
 class _Monitor:
@@ -111,6 +140,10 @@ class _Monitor:
     patience: int | None = 10
     min_delta: float | None = 0.0005
 
+    def validate(self):
+        _must_within(self.patience, 'patience epoch', 0)
+        _must_within(self.min_delta, 'patience delta', 0)
+
 @dataclasses.dataclass
 class _Phase:
     name: str = 'phase_0'
@@ -119,6 +152,16 @@ class _Phase:
     lr_scale: float | None = 1.0
     active_heads: list[str] | None = None
     frozen_heads: list[str] | None = None
+
+    def validate(self):
+        _must_within(self.num_epochs, f'{self.name}: number of epochs', 0)
+        _must_within(self.start_epoch, f'{self.name}:starting epoch', 0)
+        _must_within(self.lr_scale, 'LR scale', 0)
+        if self.start_epoch > self.num_epochs:
+            raise ValueError(
+                f'Starting epochs {self.num_epochs} for phase {self.name} is '
+                f'larger than the max number of epochs {self.num_epochs}'
+            )
 
 @dataclasses.dataclass
 class _SinglePhase:
@@ -163,6 +206,15 @@ class _OrchestrationConfig:
             case 'custom': return self.curriculum.custom.phases
             case _: raise ValueError(f'Invalid multi-phases schema: {schema}')
 
+    def validate(self):
+        self.schedule.validate()
+        self.monitor.validate()
+        if self.curriculum.schema == 'single':
+            self.single_phase.validate()
+        else:
+            for phase in self.multi_phases:
+                phase.validate()
+
 # session composite
 @dataclasses.dataclass
 class SessionConfig:
@@ -181,15 +233,38 @@ class SessionConfig:
     def validate(self):
         # mode specific requirements
         if self.mode == 'continuous':
-            pass
+            if self.orchestration.curriculum.schema != 'single':
+                raise ValueError(
+                    '[curriculum.schema] must be "single" for a continuous '
+                    'training session'
+                )
         elif self.mode == 'curriculum':
-            pass
+            if self.orchestration.curriculum.schema == 'single':
+                raise ValueError(
+                    '[curriculum.schema] must not be "single" for a curriculum'
+                    '-based training session; expected: "baseline" or "custom"'
+                )
         else:
             raise ValueError(
                 f'Invalid mode: {self.mode}, '
                 f'must be "continuous" or "curriculum"'
             )
-        # scheduler specific requirements
-        if self.engine_optim.sched_cls == 'CosAnneal':
-            if 'T_max' not in self.engine_optim.sched_args:
-                raise ValueError('missing T_max for CosAnneal')
+        # sections validation
+        self.data_loader.validate()
+        self.engine_exec.validate()
+        self.engine_optim.validate()
+        self.engine_tasks.validate()
+        self.orchestration.validate()
+
+# ------------------------------private  function------------------------------
+def _must_within(
+    value: typing.Any,
+    tag: str,
+    mmin: int | float | None = None,
+    mmax: int | float | None = None,
+) -> None:
+    if not isinstance(value, (int, float)):
+        return
+    rr = f'[{mmin}, {mmax}]'
+    if mmin and value < mmin or mmax and value > mmax:
+        raise ValueError(f'Value [{tag}] must be within {rr}, got: {value}')
